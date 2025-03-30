113a1b03ac537193f1db60fcf2cfe6fca86ea068
ramirez-alejo <a.ramirez1123@uniandes.edu.co>
2025-03-30 01:36:52 -0400

    Agregar instalacion de chrome para las pruebas del front

--- ui/app/karma.conf.js
+++ ui/app/karma.conf.js
@@ -37,11 +37,16 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
+        ]
       }
     },
     singleRun: false,
